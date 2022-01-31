f187331795834efa37603693cb9fdf612a51623d
Reaper <ahoy@barelyhuman.dev>
2022-01-31 08:08:04 +0530

    add random color generator on the docs

--- docs/src/app.js
+++ docs/src/app.js
@@ -1,63 +1,90 @@
-import './styles/global.css'
-import Input from '@zincui/input'
-import Box from '@zincui/box'
-import { useEffect, useState } from 'react'
-import { parseToHex, hexToRGB, hexToHSL } from '../../dist/index'
-import Documentation from './components/documentation.mdx'
+import "./styles/global.css";
+import Input from "@zincui/input";
+import Box from "@zincui/box";
+import { useEffect, useState } from "react";
+import { parseToHex, hexToRGB, hexToHSL } from "../../dist/index";
+import Documentation from "./components/documentation.mdx";
+import randomColor from "./lib/random-color";
 
-const defaultHex = '#18181b'
+const defaultHex = "#18181b";
 
 export const App = () => {
-  const [value, setValue] = useState(defaultHex)
+  const [value, setValue] = useState(defaultHex);
+
+  useEffect(() => {
+    const listener = (event) => {
+      if (event.code === "Space") {
+        event.preventDefault();
+        setValue(randomColor());
+      }
+    };
+
+    document.addEventListener("keypress", listener);
+
+    return () => {
+      document.removeEventListener("keypress", listener);
+    };
+  }, []);
 
   useEffect(() => {
     try {
-      document.body.style.backgroundColor = '#' + parseToHex(value)
+      document.body.style.backgroundColor = "#" + parseToHex(value);
     } catch (err) {
-      document.body.style.backgroundColor = '#' + parseToHex(defaultHex)
+      document.body.style.backgroundColor = "#" + parseToHex(defaultHex);
     }
-  }, [value])
+  }, [value]);
 
-  let asHex = ''
-  let asRGB = ''
-  let asHSL = ''
+  let asHex = "";
+  let asRGB = "";
+  let asHSL = "";
   try {
-    asHex = parseToHex(value)
-    asRGB = hexToRGB(asHex)
-    asHSL = hexToHSL(asHex)
+    asHex = parseToHex(value);
+    asRGB = hexToRGB(asHex);
+    asHSL = hexToHSL(asHex);
   } catch (err) {
-    asHex = parseToHex(defaultHex)
-    asRGB = hexToRGB(asHex)
-    asHSL = hexToHSL(asHex)
+    asHex = parseToHex(defaultHex);
+    asRGB = hexToRGB(asHex);
+    asHSL = hexToHSL(asHex);
   }
 
   return (
     <>
-      <h1>tocolor</h1>
-      <a className='text-dim' href='https://github.com/barelyhuman/tocolor'>
-        GitHub
-      </a>
+      <Box
+        flex
+        column
+        center
+        padding-8
+        style={{
+          backgroundColor: "var(--base)",
+          borderRadius: 10,
+          width: "100%",
+        }}
+      >
+        <h1>tocolor</h1>
+        <a className="text-dim" href="https://github.com/barelyhuman/tocolor">
+          GitHub
+        </a>
+      </Box>
       <Box
         style={{
           flex: 1,
-          display: 'flex',
-          marginTop: '33%',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'
+          display: "flex",
+          marginTop: "33%",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Box style={{ width: '100%' }}>
+        <Box style={{ width: "100%" }}>
           <Input
             paddingX-26
             autoFocus
-            placeholder='Enter a color value in hex, rgb, hsl'
+            large
+            placeholder="Enter a color value in hex, rgb, hsl"
             value={value}
             onChange={(e) => setValue(e.target.value)}
             style={{
-              height: 58,
-              width: '100%',
-              borderRadius: 10
+              width: "100%",
             }}
           />
 
@@ -68,20 +95,23 @@ export const App = () => {
               flex-1
               padding-8
               style={{
-                backgroundColor: 'var(--zinc-800)',
+                backgroundColor: "var(--zinc-800)",
                 borderRadius: 10,
-                width: '100%'
+                width: "100%",
               }}
             >
               <Box margin-8>
-                <span className='text-dim'>HEX:</span> #{asHex.toUpperCase()}
+                <span className="text-dim kbd">Space</span>Random Color
               </Box>
               <Box margin-8>
-                <span className='text-dim'>RGB:</span>{' '}
+                <span className="text-dim">HEX:</span> #{asHex.toUpperCase()}
+              </Box>
+              <Box margin-8>
+                <span className="text-dim">RGB:</span>{" "}
                 {`(${asRGB.r},${asRGB.g},${asRGB.b})`}
               </Box>
               <Box margin-8>
-                <span className='text-dim'>HSL:</span>{' '}
+                <span className="text-dim">HSL:</span>{" "}
                 {`(${asHSL.h},${asHSL.s},${asHSL.l})`}
               </Box>
             </Box>
@@ -94,11 +124,11 @@ export const App = () => {
         paddingX-24
         style={{
           borderRadius: 10,
-          backgroundColor: 'var(--zinc-800)'
+          backgroundColor: "var(--zinc-800)",
         }}
       >
         <Documentation />
       </Box>
     </>
-  )
-}
+  );
+};
